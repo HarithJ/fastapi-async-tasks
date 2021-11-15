4c0c7a7f349e13d358dcf7d7a4388d72817bade4
harithj <harith.bakhrani@easysolar.org>
2021-11-15 10:48:04 +0300

    Run the task in the background and trigger a webhook when ready

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from datetime import date
 import re
+import requests
+import json
 
-from fastapi import FastAPI
+from fastapi import FastAPI, BackgroundTasks
 from pydantic import BaseModel
 
 class Transaction(BaseModel):
@@ -16,7 +18,7 @@ class Merchant(BaseModel):
 
 app = FastAPI()
 
-def normalize_merchant_heuristic(tx: Transaction) -> Merchant:
+def normalize_merchant_heuristic(tx: Transaction):
     """
     Please do not focus on the implementation of this heuristic.
     For the purpose of the exercise, we will assume that the heuristic is already
@@ -28,12 +30,26 @@ def normalize_merchant_heuristic(tx: Transaction) -> Merchant:
         match = re.search("Netflix", tx.description)
 
     if match:
-        return Merchant(name="Netflix")
+        data = { "name": "Netflix" }
     else:
-        return Merchant(name="n/a")
+        data = { "name": "n/a" }
+
+    # in reality this would be a env var
+    webhook_url = "http://localhost:9898/normalize_merchant_response"
+    r = requests.post(
+        webhook_url,
+        data=json.dumps(data),
+        headers={"Content-Type": "application/json"}
+    )
 
 
 @app.post("/normalize_merchant")
-async def normalize_merchant(tx: Transaction):
-    merchant = normalize_merchant_heuristic(tx)
+async def normalize_merchant(tx: Transaction, background_tasks: BackgroundTasks):
+    background_tasks.add_task(normalize_merchant_heuristic, tx)
+    return {"message": "Matching heuristic, this may take a while"}
+
+
+@app.post("/normalize_merchant_response")
+def normalize_merchant_response(merchant: Merchant) -> Merchant:
+    print(merchant)
     return merchant
